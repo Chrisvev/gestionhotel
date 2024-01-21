348b03f5fecc6238d7754219de0920c3ce340966
christofervv97@gmail.com <christofervv97@gmail.com>
2024-01-21 21:34:19 +0100

    [ADD] Se modifico la clase hotel ahora funcional

--- main/Hotel.java
+++ main/Hotel.java
@@ -7,15 +7,9 @@ import java.util.List;
 public class Hotel {
 
     //Obtener reservas
-    public List<Reservas> obtenerReserva(Clientes cliente, Habitaciones habitacion, Date fechaInicio, Date fechaFin) {
-
-        return new ArrayList<>();
-    }
-
-    //Consultar la disponibilidad
-    public List<Clientes> obtenerCliente(Clientes cliente) {
-        
-        return new ArrayList<>();
-    }
+    public ArrayList<Clientes> listaClientes = new ArrayList<>();
+     public ArrayList<Habitaciones> listaHabitaciones = new ArrayList<>();
+      public ArrayList<Reservas> listaReservas = new ArrayList<>();
 
 }
+
